75eb4afd15485755e06922851941e5d97b12141e
Alejandro Álvarez Ayllón <alejandro.alvarez@sonarsource.com>
2024-10-30 13:45:15 +0100

    Add health information

--- peachee_dashboard/dashboard.py
+++ peachee_dashboard/dashboard.py
@@ -7,6 +7,26 @@ class Dashboard:
         self.projects = projects
         self.dates = dates
         self.table = table
+        self.date_health = self._date_health()
+        self.project_health = self._project_health()
 
     def __len__(self) -> int:
         return len(self.table)
+
+    def _date_health(self) -> Dict[datetime, int]:
+        health = dict.fromkeys(self.dates, None)
+        for date, row in zip(self.dates, self.table):
+            p = sum(task["status"] == "COMPLETED" for task in row.values()) / len(row)
+            p *= 100
+            health[date] = int(p)
+        return health
+
+    def _project_health(self) -> Dict[str, int]:
+        health = dict.fromkeys(self.projects, None)
+        for project in self.projects:
+            p = sum(
+                task["status"] == "COMPLETED" for row in self.table for task in row.values() if task["name"] == project
+            ) / len(self.table)
+            p *= 100
+            health[project] = int(p)
+        return health
